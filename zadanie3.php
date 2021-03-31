84a875be2bdcb4272e5d92fcddc5698ca133aa89
ZelonOfficial <tymoteuszpinocy@gmail.com>
2021-03-31 13:35:15 +0100

    stworzenie połączenia

--- zadanie3.php
+++ zadanie3.php
@@ -5,6 +5,11 @@ $password = "";
 $dbname = "liczby";
 $port = 3306;
 
-
+try{
+    $polaczenie = @new PDO('mysql:host='.$host.';dbname='.$dbname.';port='.$port.';charset=utf8',$username,$password);
+    echo("Połączono");
+}catch(PDOException $e){
+    
+}
 
 ?>
