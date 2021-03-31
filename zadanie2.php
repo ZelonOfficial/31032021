869dc35a7127e9240649604c453bcfc52eae0227
ZelonOfficial <tymoteuszpinocy@gmail.com>
2021-03-31 13:31:51 +0100

    stworzenie sprawdzenia

--- zadanie2.php
+++ zadanie2.php
@@ -8,6 +8,10 @@ $port = 3306;
 
 $polaczenie = @new Mysqli($host,$username,$password,$dbname,$port);
 
-
+if(mysqli_connect_errno()!=0){
+    echo("Błąd połączenia").mysqli_connect_error();
+}else{
+    echo("Połączenie udane");
+}
 
 ?>
